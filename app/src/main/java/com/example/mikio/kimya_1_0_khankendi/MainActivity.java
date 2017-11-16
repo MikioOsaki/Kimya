b6cbb0d90af11b79ff8c20b001566d47ce7982b3
gituser <mikio.osaki@posteo.de>
2017-11-16 19:07:59 +0100

    initial commit

---
+++ app/src/main/java/com/example/mikio/kimya_1_0_khankendi/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mikio.kimya_1_0_khankendi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button buttonSearch = (Button) findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "Stoff nicht gefunden", Toast.LENGTH_SHORT).show();
+    }
+}
